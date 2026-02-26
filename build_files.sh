03c13a03be8f210f0a1a22a98930dfec5281ddc0
AADITYA AGARWAL <aadityaagarwal@AADITYAs-MacBook-Air.local>
2026-02-26 19:44:44 +0530

    Fix Vercel build: Use virtual environment in build script

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,25 @@
 # build_files.sh
 
-# Install requirements with the "break-system-packages" flag to bypass the error
-pip install -r requirements.txt --break-system-packages
+echo "BUILD START"
 
-# Run migrations
-python3.9 manage.py migrate
+# 1. Create a virtual environment named 'venv'
+python3.9 -m venv venv
 
-# Seed data (If you kept this step)
-python3.9 manage.py seed_data
+# 2. Activate the virtual environment
+source venv/bin/activate
 
-# Collect static files
-python3.9 manage.py collectstatic --noinput --clear
+# 3. Install dependencies INSIDE the venv
+# Note: We don't need --break-system-packages here because we are in a venv
+pip install -r requirements.txt
+
+# 4. Make migrations
+python manage.py makemigrations
+python manage.py migrate
+
+# 5. Seed Data (If you are using this)
+python manage.py seed_data
+
+# 6. Collect Static Files
+python manage.py collectstatic --noinput --clear
+
+echo "BUILD END"
